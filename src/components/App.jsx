f90742c701726121319513a2658253f0f8b9889a
Oksana Stets <oksanastets73@gmail.com>
2022-02-20 18:24:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import { Toaster } from 'react-hot-toast';
 import NameEditor from './Phonebook/NameEditor';
 // import ContactList from './Phonebook/ContactList';
-// import Filter from './Phonebook/Filter';
+import Filter from './Phonebook/Filter';
 import { Container } from './Phonebook/PhoneBook.styled';
 
 class App extends Component {
@@ -15,7 +15,7 @@ class App extends Component {
           <h1>Phonebook</h1>
           <NameEditor />
           <h2>Contacts</h2>
-          {/* <Filter /> */}
+          <Filter />
           {/* <ContactList /> */}
         </Container>
       </>
