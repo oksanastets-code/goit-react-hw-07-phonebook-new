5e681f5a6eca0b4e9e04e36f6bdaffb12876f6d2
Oksana Stets <oksanastets73@gmail.com>
2022-02-21 15:25:58 +0200

    Update phone-book-operations.js

--- src/components/redux/PhoneBook/phone-book-operations.js
+++ src/components/redux/PhoneBook/phone-book-operations.js
@@ -1,43 +1,48 @@
 import axios from 'axios';
 import {
-    addContactRequest,
-    addContactSuccess,
-    addContactError,
-    deleteContactRequest,
-    deleteContactSuccess,
-    deleteContactError,
-    fetchContactsRequest,
-    fetchContactsSuccess,
-    fetchContactsError,
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+  fetchContactsRequest,
+  fetchContactsSuccess,
+  fetchContactsError,
 } from './phone-book-actions';
 
 axios.defaults.baseURL = 'https://6203af0f4d21c200170b9f5a.mockapi.io/api/v1/';
 
-export const fetchContacts = () => dispatch=>{ 
-    dispatch(fetchContactsRequest());
-    axios.get('/contacts')
-        .then(({ data }) => dispatch(fetchContactsSuccess(data)))
-    .catch(error =>dispatch(fetchContactsError(error)) )
-}
+export const fetchContacts = () => async dispatch => {
+  dispatch(fetchContactsRequest());
+  await axios
+    .get('/contacts')
+    .then(({ data }) => dispatch(fetchContactsSuccess(data)))
+    .catch(error => dispatch(fetchContactsError(error)));
+};
 
-export const addContact = ({ nick, number }) => dispatch => { 
-  const contact = {
-    nick: nick,
-    number: number,
-  }
-  
+export const addContact =
+  ({ nick, number }) =>
+  async dispatch => {
+    const contact = {
+      nick: nick,
+      number: number,
+    };
+
     dispatch(addContactRequest());
 
-  axios.post('/contacts', contact)
+    await axios
+      .post('/contacts', contact)
       .then(({ data }) => {
-          dispatch(addContactSuccess(data))
+        dispatch(addContactSuccess(data));
       })
-    .catch(error => dispatch(addContactError(error)))
-}
+      .catch(error => dispatch(addContactError(error)));
+  };
 
-export const deleteContact = id => dispatch => { 
-    dispatch(deleteContactRequest());
-    axios.delete(`/contacts/${id}`)
-        .then(() => dispatch(deleteContactSuccess(id)))
-    .catch(error => dispatch(deleteContactError(error)))
-}
+export const deleteContact = id => async dispatch => {
+  dispatch(deleteContactRequest());
+  await axios
+    .delete(`/contacts/${id}`)
+    .then(() => dispatch(deleteContactSuccess(id)))
+    .catch(error => dispatch(deleteContactError(error)));
+};
